aa83b6b563bd7a93c2ca1fa03108fda535d0b91f
pmg782 <mic72826@gmail.com>
2023-06-22 12:58:04 +0900

    Revert "6/22 커밋"
    This reverts commit 9d8f13ad546361294499641ff3bcb75b11e3a09d.

--- src/main/java/com/semi/mk/PlaceInfo/PlaceInfoC.java
+++ src/main/java/com/semi/mk/PlaceInfo/PlaceInfoC.java
@@ -18,7 +18,7 @@ public class PlaceInfoC extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		AccountDAO.loginCheck(request);
 		PlaceInfoDAO.getPlaceInfo(request);
-		WeatherDAO.getWeatherdao().makeWeather(request);
+		WeatherDAO.makeWeather(request);
 		request.setAttribute("contentPage","jsp/mk/detail.jsp");
 		request.getRequestDispatcher("index.jsp").forward(request, response);
 	}
