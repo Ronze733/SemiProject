ac3f3ddd5afca7392ecf6d83317f5e32ed7afdc1
Jaemin Hwang <forttrl20@gmail.com>
2023-06-28 16:14:34 +0900

    index -> NewFile

--- src/main/java/com/semi/main/HC.java
+++ src/main/java/com/semi/main/HC.java
@@ -18,7 +18,7 @@ public class HC extends HttpServlet {
 
 		AccountDAO.loginCheck(request);
 		request.setAttribute("contentPage", "./jsp/jy/recommend/recommend.jsp");
-		request.getRequestDispatcher("index.jsp").forward(request, response);
+		request.getRequestDispatcher("NewFile.jsp").forward(request, response);
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
